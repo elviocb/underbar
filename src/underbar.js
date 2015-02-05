0101e522aecf907949d7c54635028d427dc66f05
Elvio Cavalcante <elvio@madfx.com.br>
2015-02-05 15:33:14 -0200

    Fixed: underbar.js | Part 1 -> Function _.last

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,8 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    var startSlice = n > array.length ? 0 : array.length -n;
+    return n === undefined ? array[array.length -1] : array.slice(startSlice, array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
